4c289920145607b65eef8c5873fa146cae268144
jahidulislamtopu <jahudul15-2753@diu.edu.bd>
2022-09-04 08:38:22 +0530

    add new

---
+++ js/country.js
@@ -0,0 +1,43 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+
+const displayCountries = countries => {
+    const countriesContainer = document.getElementById('countries-container');
+    countries.forEach(country => {
+        //console.log(country)
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML = `
+        <h3>Name: ${country.name.common}</h3>
+        <p>Capital: ${country.capital ? country.capital[0] : ''}</p>
+        <button onclick="loadCountryDetail(${countries.cca2})">Details</button>
+        
+        `;
+        countriesContainer.appendChild(countryDiv);
+
+    })
+}
+const loadCountryDetail = (code) => {
+    //https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    //console.log('get country detail', code)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data[0]))
+
+}
+
+const displayCountryDetail = country => {
+    const countryDetail = document.getElementById('country-detail');
+    countryDetail.innerHTML = `
+    <h2>Details: ${country.name.common}</h2>
+    <img src="${country.flags.png}"
+
+    
+    `
+}
+
+loadCountries();
